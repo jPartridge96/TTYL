2b78eae9ba08ef95e10595109c7a7b6195d996fd
jPartridge96 <99056037+jPartridge96@users.noreply.github.com>
2023-03-26 01:43:28 -0400

    auth, acc and profile creation pages added

--- TTYL-Client/src/app/app.module.ts
+++ TTYL-Client/src/app/app.module.ts
@@ -7,13 +7,25 @@ import { AppComponent } from './app.component';
 import { ChatComponent } from './containers/chat/chat.component';
 import { UsernameComponent } from './components/username/username.component';
 import { AboutComponent } from './containers/about/about.component';
+import { LoginComponent } from './containers/login/login.component';
+import { AuthenticateComponent } from './containers/authenticate/authenticate.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AccountComponent } from './containers/account/account.component';
+import { ProfileComponent } from './containers/profile/profile.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatComponent,
     UsernameComponent,
-    AboutComponent
+    AboutComponent,
+    LoginComponent,
+    AuthenticateComponent,
+    NavbarComponent,
+    AccountComponent,
+    ProfileComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
