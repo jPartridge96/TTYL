8bdf89ff5f3c0d24571ca4b6cb3c02e27c14d8e9
jPartridge96 <99056037+jPartridge96@users.noreply.github.com>
2023-04-04 08:20:31 -0400

    Form validation for account creation

--- TTYL-Client/src/app/containers/account/account.component.ts
+++ TTYL-Client/src/app/containers/account/account.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-account',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent {
+  txtFirstName: any;
+  txtLastName: any;
 
+  dtpDateOfBirth: any;
+  requiredAge: number = 13;
+
+  isFormValid: boolean = false;
+
+  constructor(private router: Router) {}
+
+  txtFirstName_input(event: any) {
+    this.txtFirstName = event.target;
+    this.validateForm();
+  }
+
+  txtLastName_input(event: any) {
+    this.txtLastName = event.target;
+    this.validateForm();
+  }
+
+  dtpDateOfBirth_input(event: any) {
+    this.dtpDateOfBirth = event.target;
+    this.validateForm();
+  }
+
+  btnNext_click() {
+    this.router.navigate(['/edit-profile']);
+  }
+
+  calculateAge() {
+    let dob = new Date(this.dtpDateOfBirth.value);
+    let ageInMilliseconds = Date.now() - dob.getTime();
+    let ageInYears = ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25);
+
+    return ageInYears;
+  }
+
+  validateForm() {
+    if(!(this.txtFirstName && this.txtLastName && this.dtpDateOfBirth)) {
+      this.isFormValid = false;
+      return;
+    }
+
+    let isFirstNameValid = (this.txtFirstName.value.length >= this.txtFirstName.minLength);
+    let isLastNameValid = (this.txtLastName.value.length >= this.txtLastName.minLength);
+    let isDateOfBirthValid = this.calculateAge() >= this.requiredAge;
+
+    this.isFormValid = (isFirstNameValid && isLastNameValid && isDateOfBirthValid);
+  }
 }
